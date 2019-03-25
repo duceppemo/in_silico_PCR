daf1827007f94fe2758eb16637568020fdf3e555
Marc-Olivier Duceppe <marc-olivier.duceppe@canada.ca>
2019-03-25 16:50:53 -0400

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="in_silico_PCR",
-    version="0.0.0.1",
+    version="0.0.0.2",
     packages=find_packages(),
     author="Adam Koziol",
     author_email="adam.koziol@inspection.gc.ca",
